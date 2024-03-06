f9d6cc13533d93066a0dec1404f16cdfbcbbe8f7
lashasaata <lashasaata@gmail.com>
2024-03-06 10:41:13 -0800

    add changes in header

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,11 +1,13 @@
 function Notification() {
   return (
     <div className="flex items-center">
-      <h1 className="text-xl font-extrabold text-gray-900">Notifications</h1>
-      <span className="w-8 h-[25px] rounded-md bg-blue-800 inline-block text-center font-extrabold text-base text-white ml-[9px]">
+      <h1 className="text-xl font-extrabold text-gray-900 lg: text-2xl">
+        Notifications
+      </h1>
+      <span className="w-8 h-[25px] rounded-md bg-blue-800 inline-block text-center font-extrabold text-base text-white ml-[9px] lg: ml-[11px]">
         3
       </span>
-      <span className="text-sm font-medium text-gray-700 ml-[74px]">
+      <span className="text-sm font-medium text-gray-700 ml-[74px] lg: text-base ml-[356px]">
         Mark all as read
       </span>
     </div>
